f716f344202dd2582f3db24cd7c5e642507bd4cc
Ihar Berasneu <igor.simatic@gmail.com>
2020-10-02 10:45:17 +0300

    fead:add decision task towel sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  let newArray = [];
+
+  if (arguments.length) {
+    let num = matrix.length;
+    let array;
+
+    for (let i = 0; i < matrix.length; i++) {
+      array = matrix[i];
+      if (i % 2 === 0) {
+        for (let k = 0; k < array.length; k++) {
+          newArray.push(array[k])
+          console.log(array[k]);
+        }
+      } else {
+        for (let k = 1; k <= array.length; k++) {
+          newArray.push(array[array.length - k])
+          console.log(array[array.length - k]);
+        }
+      }
+    }
+    return newArray;
+  } else {
+    return newArray;
+  }
+
+
 }
